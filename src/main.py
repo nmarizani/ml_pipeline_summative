3f1e21f61fef0b3d82e9e2e9adc0a7858e676c46
NicolleNM <138711504+NicolleNM@users.noreply.github.com>
2025-08-03 16:52:39 +0200

    Port change

--- src/main.py
+++ src/main.py
@@ -267,4 +267,4 @@ Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
